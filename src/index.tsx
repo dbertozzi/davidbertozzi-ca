614454236105966f70c198297bb8a3770fbde57f
David Bertozzi <david.a.bertozzi@gmail.com>
2020-01-15 10:27:21 +0800

    Added font
    Added Roboto font import using webfontloader

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
+
+import WebFont from "webfontloader";
+
+WebFont.load({
+  google: {
+    families: ["Roboto", "sans-serif"]
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
