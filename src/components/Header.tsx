baec16db936f2b87ad9f288f1d4ca48ef90d1a56
David Bertozzi <david.a.bertozzi@gmail.com>
2020-01-19 08:46:00 -0500

    Added profile and cover photo to landing page
    Added prop destructuring to some components, adjusted landing/about page layout and added profile and cover photo, disabled prop-types rule in eslint because less relevant for typescript

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,14 @@ import Navbar from "./Navbar";
 
 const Header: React.FC<{
   navItems: { label: string; path: string; class: string }[];
-}> = props => {
+}> = ({ navItems }) => {
   return (
     <div className="header-wrapper">
       <div className="logo-div">
         <h1 className="name-div">David Bertozzi</h1>
         <div className="eng-div">Engineer</div>
       </div>
-      <div className="top-right-block" />
-      <Navbar navItems={props.navItems} />
+      <Navbar navItems={navItems} />
     </div>
   );
 };
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Landing: React.FC = () => {
   return (
-    <div className="content-wrapper">
-      <h2>About</h2>
+    <div className="landing-wrapper">
+      <div title="cover image" />
+      <h1>About</h1>
       <p>
         David is a graduate of University of Toronto with a degree in Mechanical
         Engineering specializing in stream options of Mechatronics and Heat and
@@ -11,9 +12,10 @@ const Landing: React.FC = () => {
         specializing in bicycle design and managing product development. David
         is a continual learner with something always on his plate and aspires to
         push limits as an engineer and in extracurricular activities through
-        dedication and consistency. Outside of engineering David plans
-        wilderness backpacking trips and is a cycling enthusiast.
+        dedication and consistency. Outside of engineering David loves the
+        outdoors and is a cycling enthusiast.
       </p>
+      <img src="/landing/profile_photo.jpg" alt="david bertozzi headshot" />
     </div>
   );
 };
